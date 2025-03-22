c944c1f4d666dda555ba0add4a5ffc86ad26907c
mitrozamit <mitrozamit@gmail.com>
2025-03-22 18:48:35 +0530

    Updated Client, Enquiry, UserMaster components and URLs

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -5,5 +5,9 @@ export const GET_ENQUIRY_BY_ID = (enquiryId: string) =>
   `api/enquiry/getEnquiryById/${enquiryId}`;
 export const ADD_ENQUIRY = "api/enquiry/addEnquiry";
 
-
+// client urls
+export const ADD_CLIENT = "api/client/add";
 export const GET_ALL_CLIENTS = "api/client/get";
+
+// user urls
+export const ADD_USER = "api/user/add";
